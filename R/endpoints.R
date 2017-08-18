352269296f68b659dc5dcc6d89efd16b0e56c709
Scott Brenstuhl <brenstsr@miamioh.edu>
2017-08-17 17:09:28 -0700

    Run and get fix

--- R/endpoints.R
+++ R/endpoints.R
@@ -1,6 +1,4 @@
 run_look <- function(connection, look_id, format="json", limit=NULL) {
-  require(httr)
-  require(jsonlite)
   request_path <- paste0("api/3.0/looks/", look_id, "/run/", format)
   check_token(connection$hostname, connection$port)
   request_response <- api_get(connection$hostname,
@@ -16,8 +14,6 @@ run_look <- function(connection, look_id, format="json", limit=NULL) {
 }
 
 get_look <- function(connection, look_id) {
-  require(httr)
-  require(jsonlite)
   request_path <- paste0("api/3.0/looks/", look_id)
 
   request_response <- api_get(connection$hostname,
